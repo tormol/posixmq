486365ef8c947f41ed3cd6ce7087b9b3b0809b5e
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2019-04-07 00:00:48 +0200

    Change receive example to use timeout

--- examples/receive.rs
+++ examples/receive.rs
@@ -1,22 +1,47 @@
 //! Receives messages from a queue, printing them and their priority to stdout.
+//! Also supports timeouts or nonblocking.
 
 extern crate posixmq;
 
 use std::env::args_os;
 use std::io::{stdout, Write};
 use std::os::unix::ffi::OsStrExt;
+use std::time::Duration;
 
 fn main() {
-    let name = args_os().skip(1).next().expect("argument required");
-    let name = name.as_bytes();
+    let mut args = args_os().skip(1);
+    let name = args.next().expect("argument required");
+    let timeout = args.next().map(|arg| {
+        let arg = arg.into_string().expect("invalid timeout");
+        if arg == "nonblocking" || arg == "drain" || arg == "available" || arg == "empty" {
+            None
+        } else if arg.ends_with("ms") {
+            Some(Duration::from_millis(arg[..arg.len()-2].parse().expect("invalid duration")))
+        } else if arg.ends_with("s") {
+            Some(Duration::from_secs(arg[..arg.len()-1].parse().expect("invalid duration")))
+        } else {
+            Some(Duration::from_secs(arg.parse().expect("invalid duration")))
+        }
+    });
 
-    let mq = posixmq::PosixMq::open(name).expect("opening failed");
+    let mut opts = posixmq::OpenOptions::readonly();
+    if timeout == Some(None) {
+        opts = *opts.nonblocking();
+    }
+    let mq = opts.open(name.as_bytes()).expect("opening failed");
 
-    let mut buf = [0; 8192];
-    loop {
-        let (priority, len) = mq.receive(&mut buf).expect("receiving failed");
-        print!("{:3}\t", priority);
-        stdout().write_all(&buf[..len]).expect("writing to stdout failed");
-        println!();
+    if let Some(Some(timeout)) = timeout {
+        let mut buf = vec![0; mq.attributes().max_msg_len];
+        while let Ok((priority, len)) = mq.receive_timeout(&mut buf, timeout) {
+            print!("{:3}\t", priority);
+            stdout().write_all(&buf[..len]).expect("writing to stdout failed");
+            println!();
+        }
+    } else {
+        for (priority, msg) in mq {
+            print!("{:3}\t", priority);
+            stdout().write_all(&msg).expect("writing to stdout failed");
+            println!();
+        }
     }
 }
