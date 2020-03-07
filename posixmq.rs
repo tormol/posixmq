8acceca9bc12af861e6a15512109f20eb65d97d7
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2020-03-07 16:21:34 +0100

    Update copyright year

--- posixmq.rs
+++ posixmq.rs
@@ -1,4 +1,4 @@
-/* Copyright 2019 Torbjørn Birch Moltu
+/* Copyright 2019, 2020 Torbjørn Birch Moltu
  *
  * Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
  * http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
